f425601ec319ec84762ef3ae0a28b8e33ae12fb5
Derek <etx313@gmail.com>
2013-07-16 22:12:14 -0400

    add CANBus Library

---
+++ libraries/CANBus/Message.cpp
@@ -0,0 +1,13 @@
+
+#include "Message.h"
+
+Message::Message(){
+    dispatch = false;
+}
+
+Message::~Message(){
+    // delete &length;
+    // delete &frame_id;
+    // delete frame_data;
+}
+
---
+++ libraries/CANBus/Message.h
@@ -0,0 +1,23 @@
+#ifndef CANMessage_H
+#define CANMessage_H
+
+#include <Arduino.h>
+#include <CANBus.h>
+
+
+class Message {
+    public:
+        Message();
+        ~Message();
+        
+        byte length;
+        unsigned short frame_id;
+        byte frame_data[8];
+    
+        unsigned int busStatus; // Intended to hold status of the bus imediately before reading of the buffer.
+        unsigned int busId;
+        bool dispatch;
+    
+};
+
+#endif
