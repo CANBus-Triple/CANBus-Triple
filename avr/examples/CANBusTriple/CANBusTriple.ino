4047f42910a7e271a6b7e7b47bd25442c9bf306a
Emanuele Iannone <emanuele@fondani.it>
2016-08-28 13:18:26 +0200

    Optimized memory usage (CANBus objects passed by reference)

--- avr/examples/CANBusTriple/CANBusTriple.ino
+++ avr/examples/CANBusTriple/CANBusTriple.ino
@@ -26,10 +26,11 @@
 #define WRITE_BUFFER_SIZE 10
 
 
-CANBus CANBus1(CAN1SELECT, CAN1RESET, 1, "Bus 1");
-CANBus CANBus2(CAN2SELECT, CAN2RESET, 2, "Bus 2");
-CANBus CANBus3(CAN3SELECT, CAN3RESET, 3, "Bus 3");
-CANBus busses[] = { CANBus1, CANBus2, CANBus3 };
+CANBus busses[] = {
+    CANBus(CAN1SELECT, CAN1RESET, 1, "Bus 1"),
+    CANBus(CAN2SELECT, CAN2RESET, 2, "Bus 2"),
+    CANBus(CAN3SELECT, CAN3RESET, 3, "Bus 3")
+};
 
 #include "Middleware.h"
 #include "Settings.h"
@@ -145,9 +146,9 @@ void loop()
     for(int i = 0; i <= activeMiddlewareLength - 1; i++) 
         activeMiddleware[i]->tick();
 
-    if (digitalRead(CAN1INT_D) == 0) readBus(CANBus1);
-    if (digitalRead(CAN2INT_D) == 0) readBus(CANBus2);
-    if (digitalRead(CAN3INT_D) == 0) readBus(CANBus3);
+    if (digitalRead(CAN1INT_D) == 0) readBus(&busses[0]);
+    if (digitalRead(CAN2INT_D) == 0) readBus(&busses[1]);
+    if (digitalRead(CAN3INT_D) == 0) readBus(&busses[2]);
 
     // Process received CAN message through middleware
     if (!readQueue.isEmpty()) {
@@ -160,7 +161,7 @@ void loop()
     while(!writeQueue.isEmpty() && !error) {
         Message msg = writeQueue.pop();
 	if (msg.dispatch) {
-	    error = !sendMessage(msg, busses[msg.busId - 1]);
+	    error = !sendMessage(msg, &busses[msg.busId - 1]);
             // When TX Failure, add back to queue
             if (error) writeQueue.push(msg);
         }
@@ -175,14 +176,14 @@ void loop()
 /*
 *  Load CAN Controller buffer and set send flag
 */
-bool sendMessage( Message msg, CANBus bus )
+bool sendMessage( Message msg, CANBus * bus )
 {
-    int ch = bus.getNextTxBuffer();
+    int ch = bus->getNextTxBuffer();
     if (ch < 0 || ch > 2) return false; // All TX buffers full
 
     digitalWrite(BOOT_LED, HIGH);
-    bus.loadFullFrame(ch, msg.length, msg.frame_id, msg.frame_data );
-    bus.transmitBuffer(ch);
+    bus->loadFullFrame(ch, msg.length, msg.frame_id, msg.frame_data );
+    bus->transmitBuffer(ch);
     digitalWrite(BOOT_LED, LOW );
 
     return true;
@@ -192,12 +193,12 @@ bool sendMessage( Message msg, CANBus bus )
 /*
 *  Read Can Controller Buffer
 */
-void readBus( CANBus bus )
+void readBus( CANBus * bus )
 {
     byte rx_status = 0x3;
     bool bufferAvailable = true;
     while(rx_status == 0x3 && bufferAvailable) {
-        rx_status = bus.readStatus();
+        rx_status = bus->readStatus();
         if (rx_status & 0x1) 
             bufferAvailable = readMsgFromBuffer(bus, 0, rx_status);
         if ((rx_status & 0x2) && bufferAvailable) 
@@ -206,13 +207,13 @@ void readBus( CANBus bus )
 }
 
 
-bool readMsgFromBuffer(CANBus bus, byte bufferId, byte rx_status)
+bool readMsgFromBuffer(CANBus * bus, byte bufferId, byte rx_status)
 {
     if (readQueue.isFull()) return false;
     Message msg;
     msg.busStatus = rx_status;
-    msg.busId = bus.busId;
+    msg.busId = bus->busId;
     msg.dispatch = false;
-    bus.readFullFrame(bufferId, &msg.length, msg.frame_data, &msg.frame_id );  
+    bus->readFullFrame(bufferId, &msg.length, msg.frame_data, &msg.frame_id );  
     return readQueue.push(msg);
 }
